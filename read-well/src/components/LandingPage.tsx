e9e77f2146d5f7d42f4af596587ec3933e5283ec
poojaiyengar123 <poojaiyengar68@gmail.com>
2025-03-29 21:42:29 -0400

    home screen

---
+++ read-well/src/components/LandingPage.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './styles.css';
+
+const LandingPage: React.FC = () => {
+    const goGallery = () => {
+        navigate('/gallery');
+    };
+
+    const goCamera = () => {
+        navigate('/camera');
+    };
+
+    return (
+        <div className="homepage">
+            <div className="title">
+                <h1>ReadWell</h1>
+            </div>
+            <div className="description">
+                <p>ReadWell is an accessibility-focused app that converts images of text into a dyslexia-friendly format with customizable fonts. It also features text-to-speech to improve reading ease and comprehension.</p>
+            </div>
+            <div className="button">
+                <button onClick={goCamera} className="camera-button">Transcribe Image</button>
+            </div>
+            <div className="button">
+                <button onClick={goGallery} className="gallery-button">View Image Gallery</button>
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
