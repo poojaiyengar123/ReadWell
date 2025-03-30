7a35c651b5be0df810c04a55661178062405c9d1
naveena-pillai <naveena.t.pillai@gmail.com>
2025-03-29 23:33:26 -0400

    removed unnecessary stuff

--- read-well/src/components/GalleryPage.tsx
+++ read-well/src/components/GalleryPage.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
 const GalleryPage: React.FC = () => {
-    const navigate = useNavigate();
-
-    const goHome = () => {
-        navigate('/');
-    };
-
-    return (
-        <div className="gallery">
-            <div className="gallery-area">
-                <div className="cards">
-                    <p>Cards</p>
-                </div>
-            </div>
+  return (
+    <div className="gallery">
+      <div className="gallery-area">
+        <div className="cards">
+          <p>Cards</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default GalleryPage;
--- read-well/src/components/Header.tsx
+++ read-well/src/components/Header.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from "/Users/poojaraghuram/Documents/VS Code/ReadWell/read-well/src/assets/readwell-logo.png";
-import './styles.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/readwell-logo.png";
+import "./styles.css";
 
 const Header: React.FC = () => {
-    return (
-        <header className="header">
-            <Link to="/">
-                <img src={logo} alt="Logo" className="logo" />
-            </Link>
-        </header>
-    );
+  return (
+    <header className="header">
+      <Link to="/">
+        <img src={logo} alt="Logo" className="logo" />
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
